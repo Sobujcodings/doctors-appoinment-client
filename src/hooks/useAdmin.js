dc9c0549a6c60efcce38cf9292852c85849a77d0
Asif Hossain <asifh9277@gmail.com>
2023-05-20 19:42:53 +0600

    project

---
+++ src/hooks/useAdmin.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+export const useAdmin = (email) => {
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [isAdminLoading, setIsAdminLoading] = useState(true);
+  useEffect(() => {
+    if (email) {
+      fetch(
+        `https://doctors-portal-server-instasif.vercel.app/users/admin/${email}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          if (data) {
+            setIsAdmin(data.isAdmin);
+            setIsAdminLoading(false);
+          }
+        });
+    }
+  }, [email]);
+  return [isAdmin, isAdminLoading];
+};
